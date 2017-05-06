9db6f66a844fc25b2ea68dbfd40089107aac67f8
Alex Troncoso <Alex Troncoso>
2017-05-06 16:36:10 -0300

    incorporacion de archivos

---
+++ application/views/master/admin/head_admin.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+  <head>
+  <title><?= $titulo ?></title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!--  <link rel="shortcut icon" href="<?=base_url()?>assets/img/favicon.ico" type="image/x-icon" /> -->
+
+  <link rel="stylesheet" href="<?=base_url()?>assets/css/font-awesome.min.css" type="text/css" media="screen">
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/admin.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/materialize.min.css" media="screen">
+
+  <script type="text/javascript" src="<?=base_url()?>assets/js/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="<?=base_url()?>assets/js/jquery.materialize-autocomplete.min.js"></script>
+  <script type="text/javascript" src="<?=base_url()?>assets/js/jquery.validate.min.js"></script>
+  <script type="text/javascript" src="<?=base_url()?>assets/js/materialize.min.js"></script>
+</head>
