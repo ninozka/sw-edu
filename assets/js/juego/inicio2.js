05e2926a122e77e1850f7b48cae9a870de1d2661
ninozka <ninozka.93@gmail.com>
2017-05-27 18:16:02 -0400

    anterior y siguiente funcionando

--- assets/js/juego/inicio2.js
+++ assets/js/juego/inicio2.js
@@ -9,11 +9,22 @@ Inicio2.prototype = {
 
     },
 
+    preload: function(){
+        this.game.load.spritesheet('btn-a','../assets/buttons/btn-a.png', 58, 58);
+        this.game.load.spritesheet('btn-s','../assets/buttons/btn-s.png', 58, 58);
+
+    },
+
     create: function(){
     
-        
+        var anterior = this.game.add.button((window.innerWidth*0.01),(window.innerHeight*0.53),'btn-a',this.previous,this,2,1,0);
+        var siguiente = this.game.add.button((window.innerWidth*0.593),(window.innerHeight*0.53),'btn-s','',this,2,1,0);
         this.game.stage.backgroundColor = "#fff";
         text = this.game.add.text(100, 70, "hola", { font: "22px Arial", fill: "#ff0044" });
 
+    },
+
+    previous: function(){
+        this.game.state.start("Inicio1");
     }
 }
